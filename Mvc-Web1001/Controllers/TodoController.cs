f5901ce32fa214bafda57bc5704c803b293e179f
Mariam Zahid <mzahidmalhi@hotmail.com>
2023-08-09 01:09:48 -0400

    added views for edit delete

--- Mvc-Web1001/Controllers/TodoController.cs
+++ Mvc-Web1001/Controllers/TodoController.cs
@@ -3,3 +3,21 @@ public IActionResult Index()
     var todoItems=_context.TodoItems.TodoList();
     return View(todoItems);
 }
+
+public IActionResult Create()
+{
+    var todoItems=_context.TodoItems.TodoList();
+    return View(todoItems);
+}
+
+public IActionResult Edit()
+{
+    var todoItems=_context.TodoItems.TodoList();
+    return View(todoItems);
+}
+
+public IActionResult Delete()
+{
+    var todoItems=_context.TodoItems.TodoList();
+    return View(todoItems);
+}
