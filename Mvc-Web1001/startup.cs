30e3e9d193f0da28b4ffd52418ffa9da2c1b7b20
Mariam Zahid <mzahidmalhi@hotmail.com>
2023-08-09 01:21:44 -0400

    Add configuration methods

--- Mvc-Web1001/startup.cs
+++ Mvc-Web1001/startup.cs
@@ -1,2 +1,9 @@
 services.AddDbContext<AddDbContext>(options=>
 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
